19087f5332dca6d99044f8be5e360ee0999db41c
teoshuqi <77791600+teoshuqi@users.noreply.github.com>
2022-03-28 22:35:28 +0800

    used dcgan class

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
 import json
 import utils
 import glob
-from models import Generator, Discriminator
+from models import DCGAN
 from data import MINST
 
 with open('config.json', 'r') as f_:
@@ -11,31 +13,22 @@ with open('config.json', 'r') as f_:
 num_examples_to_generate = config['num_examples_to_generate']
 noise_dim = config['noise_dim']
 DATANAME = config['dataname']
+model_dir = config['model_dir']
+predict_dir = config['predict_dir']
+stats_dir = config['stats_dir']
 
-generator = Generator(config['generator'])
-discriminator = Discriminator(config['discriminator'])
-if len(glob.glob(f"checkpoints\\*_{DATANAME}.h5")) > 1:
-    generator.model.load_weights(f'./checkpoints/generator_{DATANAME}.h5')
-    discriminator.model.load_weights(f'./checkpoints/discriminator_{DATANAME}.h5')
+if __name__ == "__main__":
+    dcgan = DCGAN(config)
+    if len(glob.glob(f"{model_dir}\\{DATANAME}\\*_{DATANAME}.h5")) > 1:
+        dcgan.load_weights()
 
-test_input = tf.random.normal([num_examples_to_generate, noise_dim])
+    test = tf.random.normal([num_examples_to_generate, noise_dim])
+    train_dataset = MINST()
 
-print('Initial')
-utils.generate_and_save_images(generator.model, test_input, f'{DATANAME}_0000.png')
+    dcgan.predict(test, f'{DATANAME}_0000.png')
 
-print('Train')
-train_dataset = MINST()
-history = utils.train(train_dataset, test_input, generator, discriminator)
+    print(f'Start training DCGAN for {DATANAME} dataset')
+    history = dcgan.train(train_dataset, test)
 
-print('Final')
-utils.generate_and_save_images(generator.model, test_input, f'{DATANAME}_final.png')
-
-utils.saveImagesAsGIF(f'./stats/dcgan_{DATANAME}.gif')
-utils.saveAndPlotModelPerformance(history, f'./stats/dcgan_{DATANAME}')
-
-
-generator_pred = Generator(config['generator'])
-discriminator_pred = Discriminator(config['discriminator'])
-generator_pred.model.load_weights(f'./checkpoints/generator_{DATANAME}.h5')
-discriminator_pred.model.load_weights(f'./checkpoints/discriminator_{DATANAME}.h5')
-utils.generate_and_save_images(generator_pred.model, test_input, f'{DATANAME}_pred.png')
+    utils.saveImagesAsGIF()
+    dcgan.saveModelPerformance()
