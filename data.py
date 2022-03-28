83a93ee655efd2fbe32a849ec10d90c75340bc5d
teoshuqi <77791600+teoshuqi@users.noreply.github.com>
2022-03-28 22:35:44 +0800

    changed data normalisation

--- data.py
+++ data.py
@@ -11,7 +11,7 @@ BATCH_SIZE = config['batch_size']
 def MINST():
     (train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
     train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
-    train_images = train_images/255  # Normalize the images to [-1, 1]
+    train_images = (train_images- 127.5) / 127.5  # Normalize the images to [-1, 1]
     # Batch and shuffle the data
     train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
     return train_dataset
